a3ccfcbea95abb7f83edc56901094bcc5c2b9d07
макс <@->
2021-10-13 20:33:16 +0300

    Добавьте файлы проекта.

---
+++ TRPO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+                Console.WriteLine("Hello, Yuri Mikhailovich!");
+            Console.ReadLine();
+        }
+    }
+}
